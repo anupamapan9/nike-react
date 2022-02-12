436385a13118af9f546fe74c39a634a1ce7919ce
anupamapan9 <anupamapan0905@gmail.com>
2022-02-12 12:38:36 +0600

    js added

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("text-change-btn").addEventListener('click', function () {
+    const headerText = document.getElementById("header-text")
+    headerText.innerText = "I am a panguine"
+})
